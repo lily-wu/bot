26186fee15a05dfa0e273ec0cee2a7ee2fd87cb7
edwardbottom <31777866+edwardbottom@users.noreply.github.com>
2018-01-03 08:11:13 -0600

    tinder configured with maven
    tinder library with mavin

---
+++ src/main/java/tinder/bot/App.java
@@ -0,0 +1,13 @@
+package tinder.bot;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
